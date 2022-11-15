ffb9cbca0a57b6e433fe5e0da4f869b215310c9b
euclidgame <1500880529@qq.com>
2022-11-15 14:01:13 +0800

    add SimpleProgram

--- src/main/java/equivalence/judgement/program/SimpleProgram.java
+++ src/main/java/equivalence/judgement/program/SimpleProgram.java
@@ -1,4 +1,33 @@
 package equivalence.judgement.program;
 
-public class SimpleProgram {
+import equivalence.judgement.type.Type;
+
+import java.util.List;
+
+public class SimpleProgram implements Program {
+    private final String path;
+
+    public SimpleProgram(String relativePath) {
+        path = relativePath;
+    }
+
+    @Override
+    public String relativePath() {
+        return path;
+    }
+
+    @Override
+    public String absolutePath() {
+        return null;
+    }
+
+    @Override
+    public List<Type> inputTypes() {
+        return null;
+    }
+
+    @Override
+    public String compile() {
+        return null;
+    }
 }
